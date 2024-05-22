93c08657b0b3ed9c68a4e814be84bbbf1f31c43a
Hala318 <145948383+Hala318@users.noreply.github.com>
2024-05-22 18:02:07 +0300

    Update timesformer.py

--- models/backbones/timesformer.py
+++ models/backbones/timesformer.py
@@ -1,37 +1,18 @@
 import torch
 from transformers import AutoImageProcessor, TimesformerForVideoClassification
-from torch import nn
+from transformers import VivitImageProcessor, VivitForVideoClassification
 
-# Load the Timesformer model
-model = TimesformerForVideoClassification.from_pretrained("facebook/timesformer-base-finetuned-k600")
+model = VivitForVideoClassification.from_pretrained("google/vivit-b-16x2-kinetics400")
 
-# Access the backbone of the model
-backbone = model.timesformer
+backbone = model.vivit
 
+def timesFormer(pretrained: bool = False,  **kwargs: any):
+    # b = backbone(
+    #     **kwargs,
+    # )
+    config = model.config
 
-class TimesformerFeatureExtractor(nn.Module):
-    def __init__(self, backbone):
-        super(TimesformerFeatureExtractor, self).__init__()
-        self.backbone = backbone
-
-    def forward(self, pixel_values, **kwargs):
-        outputs = self.backbone(pixel_values=pixel_values, **kwargs)
-        sequence_output = outputs.last_hidden_state[:, 0]  # Assuming you want the representation of the first token
-        return sequence_output
-
-# Create the feature extractor model
-feature_extractor = TimesformerFeatureExtractor(backbone)
-
-# Save the modified model without the classification head
-torch.save(feature_extractor.state_dict(), "timesformer_feature_extractor.pth")
-
-def timesFormer(pretrained: bool = False, in_channels: int = 1, **kwargs: any) -> TimesformerFeatureExtractor:
-    model = feature_extractor(
-        **kwargs,
-    )
-    # if pretrained:
-    #     state_dict = load_state_dict_from_url(model_urls["MobileNet"])
-    #     state_dict = _modify_weights(state_dict, in_channels)
-    #     model.load_state_dict(state_dict, strict=True)
-
-    return model
+    # Check for the number of frames
+    num_frames = config.num_frames
+    print(f"The model expects {num_frames} frames per video clip.")
+    return backbone
